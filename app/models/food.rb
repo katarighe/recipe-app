8038713f5cfecab50b1c495da65cf1e5411f979d
katarighe <m.ighe@hotmail.com>
2023-11-14 21:45:06 +0300

    Fix linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,7 +2,7 @@ class Food < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :inventory_foods, dependent: :destroy
 
-  validates :name, presence: true, length: {maximum: 250}
+  validates :name, presence: true, length: { maximum: 250 }
   validates :measurement_unit, presence: true
-  validates :price_dollars, presence: true, numericality: {greater_than: 0.0}
+  validates :price_dollars, presence: true, numericality: { greater_than: 0.0 }
 end
