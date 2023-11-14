1fa4b76a0dfa5b3f8177b8c0d12d0fa22492e097
WHB901 <abdulwhbhussain@gmail.com>
2023-11-14 14:14:11 +0500

    Schema and database created

--- commands.rb
+++ commands.rb
@@ -5,21 +5,21 @@ rails generate migration CreateUsers name:string
 
 rails generate migration CreateInventories name:string user:references
 
-rails generate migration CreateRecipes name:string preparation_time:decimal cooking_time:decimal description:text public:boolean user:references
+# rails generate migration CreateRecipes name:string preparation_time:decimal cooking_time:decimal description:text public:boolean user:references
 
-rails generate migration CreateFoods name:string measurement_unit_kg:decimal price:decimal
+# rails generate migration CreateFoods name:string measurement_unit_kg:decimal price:decimal
 
-rails generate migration CreateInventoryFoods quantity_kg:decimal inventory:references food:references
+# rails generate migration CreateInventoryFoods quantity_kg:decimal inventory:references food:references
 
-rails generate migration CreateRecipeFoods quantity_kg:decimal recipe:references food:references
+# rails generate migration CreateRecipeFoods quantity_kg:decimal recipe:references food:references
 
-rails db:migrate
+# rails db:migrate
 
-rails db:seed
+# rails db:seed
 
-rails db:rollback
+# rails db:rollback
 
-rails db:migrate RAILS_ENV=test
+# rails db:migrate RAILS_ENV=test
 
 
 
