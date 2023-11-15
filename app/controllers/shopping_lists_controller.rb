80adb639793767af111f5b3f3335967d837359b9
katarighe <m.ighe@hotmail.com>
2023-11-15 13:36:27 +0300

    Fix typo

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -4,7 +4,7 @@ class ShoppingListsController < ApplicationController
   def index
     @recipe = Recipe.find(params[:recipe_id])
     @inventory = Inventory.find(params[:inventory][:inventory_id])
-    @shopping_list = shoping_list
+    @shopping_list = shopping_list
     @total_foods_value = total_value_of_food_needed(@shopping_list)
   end
 end
