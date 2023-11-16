6f795f082f1f7cde3792bbc93d937e2f4f65ee13
WHB901 <abdulwhbhussain@gmail.com>
2023-11-16 13:28:23 +0500

    Shopping List View and Controller

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   resources :inventory_foods, only: [:new, :create, :destroy]
 
   # Create Route for Shopping List
-  get '/shopping_list?recipe_id=params[:recipe_id]&inventory_id=params[:inventory][:inventory_id]', to: 'shopping_lists#index'
+  get '/shopping_lists', to: 'shopping_lists#index', as: 'shopping_lists'
 
   # Defines the root path route ("/")
   # root "posts#index"
